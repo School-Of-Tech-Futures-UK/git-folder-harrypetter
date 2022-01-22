2f6a8b9e9336848a582316752d25a663e873e848
Harry Petter <harry.petter@accenture.com>
2022-01-22 00:01:32 +0000

    Data persistent after reset

--- Connect 4/connect4.js
+++ Connect 4/connect4.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable eqeqeq */
+/* eslint-disable prefer-const */
+/* eslint-disable camelcase */
 const tag = document.getElementById('header')
-tag.innerText = "Connect 4"
+tag.innerText = 'Connect 4'
 let turn = 0
 let player1 = 'red'
 let win_red = false
@@ -7,11 +11,9 @@ let win_yellow = false
 let nobody = false
 let score = 0
 let game = 0
-let username = 'Harry'
 let receivedData = []
-let red_name = "Anonymous (Red)"
-let yellow_name = "Anonymous (Yellow)"
-
+let red_name = 'Anonymous (Red)'
+let yellow_name = 'Anonymous (Yellow)'
 
 let grid = [
   [null, null, null, null, null, null, null],
@@ -22,11 +24,10 @@ let grid = [
   [null, null, null, null, null, null, null]
 ]
 
-const takeNames = () =>{
-  red_name = document.getElementById('rname').value + " (Red)"
-  yellow_name = document.getElementById('yname').value + " (Yellow)"
+const takeNames = () => {
+  red_name = document.getElementById('rname').value + ' (Red)'
+  yellow_name = document.getElementById('yname').value + ' (Yellow)'
 }
-
 
 const takeTurn = (e) => {
   const id = e.target.id // rowX-colY
@@ -69,27 +70,24 @@ const getLowestAvailableRowInColumn = (columnNumber, grid) => {
   }
   return null
 }
- 
-
 
 const upload = (winner_name, score) => {
-fetch('http://localhost:3000/highscore', { // Your POST endpoint
-  method: 'POST',
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({ Username : winner_name, Score :score})
-})
-.then(function(response) {
-  if(response.ok) {
-      return;
-  }throw new Error('Request failed.');
+  fetch('http://localhost:3000/highscore', { // Your POST endpoint
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ Username: winner_name, Score: score })
   })
-  .catch(function(error) {
-  console.log(error);
-  });
+    .then(function (response) {
+      if (response.ok) {
+        return
+      } throw new Error('Request failed.')
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
 }
-
 
 const download = async () => {
   console.log('downloading data')
@@ -102,8 +100,8 @@ const download = async () => {
 const checkRow = () => {
   for (let i = 0; i < 6; i++) {
     for (let j = 0; j < 7; j++) {
-      if (grid[i][j] == grid[i][j + 1] && 
-          grid[i][j] == grid[i][j + 2] && 
+      if (grid[i][j] == grid[i][j + 1] &&
+          grid[i][j] == grid[i][j + 2] &&
           grid[i][j] == grid[i][j + 3]) {
         if (grid[i][j] == 'red') {
           win_red = true
@@ -120,7 +118,7 @@ const checkColumn = () => {
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 7; j++) {
       if (grid[i][j] == grid[i + 1][j] &&
-          grid[i][j] == grid[i + 2][j] && 
+          grid[i][j] == grid[i + 2][j] &&
           grid[i][j] == grid[i + 3][j]) {
         if (grid[i][j] == 'red') {
           win_red = true
@@ -133,75 +131,71 @@ const checkColumn = () => {
   }
 }
 
-const checkDiagonal1 = () =>{
+const checkDiagonal1 = () => {
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 7; j++) {
-      if (grid[i][j] == grid[i + 1][j + 1] && 
-          grid[i][j] == grid[i + 2][j + 2] && 
+      if (grid[i][j] == grid[i + 1][j + 1] &&
+          grid[i][j] == grid[i + 2][j + 2] &&
           grid[i][j] == grid[i + 3][j + 3]) {
         if (grid[i][j] == 'red') {
           win_red = true
         }
         if (grid[i][j] == 'yellow') {
           win_yellow = true
-        }      
+        }
       }
     }
   }
 }
 
-const checkDiagonal2 = () =>{
+const checkDiagonal2 = () => {
   for (let i = 0; i < 3; i++) {
     for (let j = 7; j > 2; j--) {
-      if (grid[i][j] == grid[i + 1][j - 1] && 
-          grid[i][j] == grid[i + 2][j - 2] && 
+      if (grid[i][j] == grid[i + 1][j - 1] &&
+          grid[i][j] == grid[i + 2][j - 2] &&
           grid[i][j] == grid[i + 3][j - 3]) {
         if (grid[i][j] == 'red') {
           win_red = true
         }
         if (grid[i][j] == 'yellow') {
           win_yellow = true
-        }  
+        }
       }
     }
   }
 }
 
-const winnerMessage = () =>{
+const winnerMessage = () => {
   if (win_red == true) {
     const hiddenText = document.getElementById('winner-display')
     hiddenText.style.display = 'block'
     hiddenText.style.backgroundColor = 'red'
     hiddenText.textContent = 'The winner is red!'
     hiddenText.style.color = 'white'
-    score = 42-turn
-    upload(red_name,score)
+    score = 42 - turn
+    upload(red_name, score)
     download()
-    setTimeout(() => { printHighScores()
-    }, 200);
-
-
-
+    setTimeout(() => {
+      printHighScores()
+    }, 200)
   } else if (win_yellow == true) {
     const hiddenText = document.getElementById('winner-display')
     hiddenText.style.display = 'block'
     hiddenText.style.backgroundColor = 'yellow'
     hiddenText.textContent = 'The winner is yellow!'
     hiddenText.style.color = 'black'
-    score = 42-turn
+    score = 42 - turn
     upload(yellow_name, score)
     download()
-    setTimeout(() => { printHighScores()
-      }, 200);
-
-
+    setTimeout(() => {
+      printHighScores()
+    }, 200)
   } else if (nobody == true) {
     const hiddenText = document.getElementById('winner-display')
     hiddenText.style.display = 'block'
     hiddenText.textContent = 'Nobody wins'
     hiddenText.style.backgroundColor = 'blue'
     hiddenText.style.color = 'white'
-
   } else {
     const hiddenText = document.getElementById('winner-display')
     hiddenText.style.display = 'none'
@@ -212,20 +206,19 @@ const winnerMessage = () =>{
 
 const drawBoard = (lowestAvailableRow, colNum) => {
   if (grid[lowestAvailableRow][colNum - 1] == 'red') {
-    document.getElementById(`row${lowestAvailableRow + 1}-col${colNum}`).style.backgroundColor = "red"
+    document.getElementById(`row${lowestAvailableRow + 1}-col${colNum}`).style.backgroundColor = 'red'
   } else if (grid[lowestAvailableRow][colNum - 1] == 'yellow') {
-    document.getElementById(`row${lowestAvailableRow + 1}-col${colNum}`).style.backgroundColor = "yellow"
+    document.getElementById(`row${lowestAvailableRow + 1}-col${colNum}`).style.backgroundColor = 'yellow'
   }
 }
 
 const printHighScores = () => {
-  console.log(receivedData)
-  var highscoreboard = document.getElementById('highscore')
-  highscoreboard.innerHTML = ""
+  let highscoreboard = document.getElementById('highscore')
+  highscoreboard.innerHTML = ''
   highscoreboard.style.display = 'block'
-  for(var i=0; i < receivedData.length; i++){
-    if (i < 10){
-      highscoreboard.innerHTML += "<p>"+ receivedData[i].Username +" : " + receivedData[i].Score+"</p>"
+  for (let i = 0; i < receivedData.length; i++) {
+    if (i < 10) {
+      highscoreboard.innerHTML += '<p>' + receivedData[i].Username + ' : ' + receivedData[i].Score + '</p>'
     }
   }
 }
@@ -239,14 +232,13 @@ const resetGame = () => {
     [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null]
   ]
-  
+
   for (let i = 1; i <= 6; i++) {
     for (let j = 1; j <= 7; j++) {
-      document.getElementById(`row${i}-col${j}`).style.backgroundColor = "white"
+      document.getElementById(`row${i}-col${j}`).style.backgroundColor = 'white'
     }
   }
 
- 
   player1 = 'red'
   turn = 0
   win_yellow = false
@@ -256,4 +248,3 @@ const resetGame = () => {
   winnerMessage()
   game++
 }
-
--- Connect 4/server.js
+++ Connect 4/server.js
@@ -1,68 +1,43 @@
-const express = require('express');
-const app = express();
+/* eslint-disable node/no-path-concat */
+/* eslint-disable no-undef */
+const express = require('express')
+const app = express()
 app.use(express.json())
 const cors = require('cors')
-let highscores = []
+const highscores = []
 fs = require('fs')
 app.use(cors())
-    
 
-
-var bodyParser = require('body-parser');
-const EnumerableOwnProperties = require('es-abstract/2017/EnumerableOwnProperties');
-app.use( bodyParser.json() ); // to support JSON-encoded bodies
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()) // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-extended: true
-}));
-app.use(express.json()); // to support JSON-encoded bodies
+  extended: true
+}))
+app.use(express.json()) // to support JSON-encoded bodies
 app.use(express.static(__dirname + '/'))
 
-app.get('/connect',function(req,res) {
-res.sendFile(__dirname + '/index.html');
-});
-
-
+app.get('/connect', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
 app.post('/highscore', (req, res) => {
-    const winnerscore = req.body
-    res.json('Data received by server')
-    highscores.push(winnerscore)
-    const sortByScore = (a,b) => b.Score-a.Score
-    highscores.sort(sortByScore)
-    console.log(highscores)
-    const fileData = JSON.parse(fs.readFileSync('highscores.json'))
-    fs.writeFileSync('highscores.json', JSON.stringify(highscores, null, 2));
-    fileData.push(highscores)
-    
-    
+  const winnerscore = req.body
+  res.json('Data received by server')
+  const sortByScore = (a, b) => b.Score - a.Score
+  let fileData = JSON.parse(fs.readFileSync('highscores.json'))
+  fileData.push(winnerscore)
+  console.log(fileData)
+  fileData.sort(sortByScore)
+  fs.writeFileSync('highscores.json', JSON.stringify(fileData));
 })
-
-// require('fs').appendFile(
-
-//     './highscores.json',
-
-//     JSON.stringify(highscores),
-
-//     function (err) {
-//         if (err) {
-//             console.error('Could not save file');
-//         }
-//     }
-// );
-
-
-
 
 app.get('/highscore', (req, res) => {
-    const fs = require('fs');
-    fs.readFile('highscores.json', (err, data) => {
-        if (err) throw err;
-        let returnscores = JSON.parse(data);;
-        res.json(returnscores)
-        })
-
+  const fs = require('fs')
+  fs.readFile('highscores.json', (err, data) => {
+    if (err) throw err
+    const returnscores = (JSON.parse(data))
+    res.json(returnscores)
+  })
 })
 
-
-
-app.listen(3000,() => {console.log("Server started")})
+app.listen(3000, () => { console.log('Server started') })
